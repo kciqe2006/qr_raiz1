14953e7f7074b69551c29c19fb3288e01d30b289
kciqe2006 <131399858+kciqe2006@users.noreply.github.com>
2025-03-04 12:45:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Esto obtiene el puerto desde la variable de entorno
+    app.run(host="0.0.0.0", port=port)  # Asegúrate de que esté escuchando en 0.0.0.0
 import qrcode
 
 app = Flask(__name__)
