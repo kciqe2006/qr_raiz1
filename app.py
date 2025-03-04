dc7aa00911f493a4c3c9560204a216171d22f3e0
kciqe2006 <131399858+kciqe2006@users.noreply.github.com>
2025-03-04 14:14:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template, request, send_file
 import qrcode
 from io import BytesIO
@@ -66,6 +65,4 @@ def generate_qr():
     return send_file(img_io, mimetype='image/png')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Usar el puerto proporcionado por Render
-    app.run(host='0.0.0.0', port=port, debug=True)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
